35b769a88a758fad3ba9206478860b1a7ea21821
AshleyL45 <laboryashley@gmail.com>
2025-02-24 11:25:39 +0100

    first commit

---
+++ src/main/java/com/example/odyssea/OdysseaApplication.java
@@ -0,0 +1,13 @@
+package com.example.odyssea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdysseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdysseaApplication.class, args);
+	}
+
+}
